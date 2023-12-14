7aed44978018036493e68e4f0aa4cfc5169b1732
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-14 19:44:59 +0200

    add: create-board action with it's schemas and types

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,9 @@
 import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
   return (
     <div>
       <form action={create}>
@@ -14,6 +16,11 @@ const OrganizationIdPage = () => {
         />
         <Button type="submit">Sumbit</Button>
       </form>
+      <div>
+        {boards.map((board) => (
+          <div key={board.id}>{board.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
