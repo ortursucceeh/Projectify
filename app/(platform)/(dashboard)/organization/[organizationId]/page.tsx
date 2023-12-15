3b45b39a85eede26d52f46b3990bead4bc51713d
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-15 15:07:02 +0200

    fix: cleared organizationid page and it's files

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,4 @@
-import { db } from "@/lib/db";
-import Form from "./form";
-
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-  return (
-    <div>
-      <Form />
-      <div>
-        {boards.map((board) => (
-          <div key={board.id}>{board.title}</div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 export default OrganizationIdPage;
