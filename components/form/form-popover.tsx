4c247eea57e7065428bddfb63d66e58eb77a816c
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-15 20:16:22 +0200

    add: promodal

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -17,6 +17,7 @@ import { toast } from "sonner";
 import FormPicker from "./form-picker";
 import { ElementRef, useRef } from "react";
 import { useRouter } from "next/navigation";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -32,6 +33,7 @@ const FormPopover = ({
   sideOffset = 0,
 }: FormPopoverProps) => {
   const router = useRouter();
+  const proModal = useProModal();
 
   const closeRef = useRef<ElementRef<"button">>(null);
 
@@ -43,6 +45,7 @@ const FormPopover = ({
     },
     onError: (error) => {
       toast.error(error);
+      proModal.onOpen();
     },
   });
 
