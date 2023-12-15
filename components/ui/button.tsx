33bc4491a6552a1cc8828884b6a013de3745178c
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-15 21:30:24 +0200

    add: boardid navbar and title form without action

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         primary: "bg-blue-700 text-primary-foreground hover:bg-blue-700/90",
+        transparent:
+          "bg-transparent text-white hover:bg-white/20 border focus:border-white",
       },
       size: {
         default: "h-10 px-4 py-2",
