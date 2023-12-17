022dc582b432f70888aabbbeb29a4248d02196f8
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-17 14:54:15 +0200

    add: card-item and listing it in list-item

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { ListWithCards } from "@/types";
 import ListHeader from "./list-header";
 import CardForm from "./card-form";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -32,6 +33,16 @@ const ListItem = ({ data, index }: ListItemProps) => {
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader data={data} onAddCard={enableEditing} />
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0"
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card} />
+          ))}
+        </ol>
         <CardForm
           listId={data.id}
           ref={textareaRef}
