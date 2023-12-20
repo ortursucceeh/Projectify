8ac804fbb0d32b9babbca8489ec382a538487350
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-20 17:05:10 +0200

    add: dialog for card-item

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
+import { Draggable } from "@hello-pangea/dnd";
+
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface CardItemProps {
   data: Card;
@@ -9,6 +11,8 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -17,7 +21,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
-          onClick={() => {}}
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
