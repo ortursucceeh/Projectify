5d8c78dc15be8f957ee5e5ecdb88af4308da5c19
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-12-21 22:09:45 +0200

    remove: billing info

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -47,7 +47,6 @@ const BoardList = async () => {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
             <Hint
               sideOffset={40}
               description={`Free workspaces can have up to 5 open boards.`}
--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -23,10 +23,10 @@ const Info = () => {
         />
       </div>
       <div className="space-y-1">
-        <p className="font-semibold text-xl">{organization?.name}</p>
-        <div className="flex items-center text-xs text-muted-foreground">
+        <p className="font-semibold text-3xl">{organization?.name}</p>
+        {/* <div className="flex items-center text-xs text-muted-foreground">
           <CreditCard className="h-3 w-3 mr-1" /> Free
-        </div>
+        </div> */}
       </div>
     </div>
   );
